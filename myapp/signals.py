51a1115e7765c9c6b42e8a0bbf9298915db7f487
merlinsaja19 <163329158+merlinsaja19@users.noreply.github.com>
2025-05-07 01:12:45 +0530

    Auto-create superuser on first migrate

---
+++ myapp/signals.py
@@ -0,0 +1,14 @@
+from django.contrib.auth import get_user_model
+from django.db.models.signals import post_migrate
+from django.dispatch import receiver
+
+@receiver(post_migrate)
+def create_default_superuser(sender, **kwargs):
+    User = get_user_model()
+    if not User.objects.filter(username='admin').exists():
+        User.objects.create_superuser(
+            username='admin',
+            email='admin@example.com',
+            password='adminpassword'
+        )
+        print("Superuser 'admin' created!")
